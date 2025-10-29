282f547c5b5b2cee28edc1e9f58539f1cbc1f7c8
Rohan Shah <rohanshah945@gmail.com>
2025-10-30 02:49:27 +0530

    add: cors middleware

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Enable CORS
+app.use(cors());
 
 // Load graph data
 const graphData = JSON.parse(
