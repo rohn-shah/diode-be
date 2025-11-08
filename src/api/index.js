3cf8f17678038410e2dda72d29bc677e8e7076d8
Rohan Shah <rohanshah945@gmail.com>
2025-11-08 07:03:36 +0530

    update routes to singular convention.

--- src/api/index.js
+++ src/api/index.js
@@ -10,21 +10,21 @@ const Employee = require('../models/Employee');
 const router = express.Router();
 
 // Company endpoints
-router.use('/companies', raExpressMongoose(Company, {
+router.use('/company', raExpressMongoose(Company, {
   q: ['name', 'email', 'industry'],
   allowedRegexFields: ['name', 'industry'],
   useLean: false
 }));
 
 // User endpoints (belongs to Company)
-router.use('/users', raExpressMongoose(User, {
+router.use('/user', raExpressMongoose(User, {
   q: ['firstName', 'lastName', 'email'],
   allowedRegexFields: ['firstName', 'lastName', 'department'],
   useLean: false
 }));
 
 // Employee endpoints (independent admins)
-router.use('/employees', raExpressMongoose(Employee, {
+router.use('/employee', raExpressMongoose(Employee, {
   q: ['firstName', 'lastName', 'email', 'employeeId'],
   allowedRegexFields: ['firstName', 'lastName'],
   useLean: false
